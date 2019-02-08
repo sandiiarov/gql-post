b922a8b81a1fbb63284c9b62082629cb486b4000
Alex Sandiiarov <sandiyarov@gmail.com>
2019-02-08 14:49:15 -0600

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { ApolloServer, gql } from 'apollo-server-express';
-import express from 'express';
+import { ApolloServer, gql } from 'apollo-server';
 import fs from 'fs';
 import { resolvers } from './resolvers';
-
-const app = express();
 
 const server = new ApolloServer({
   typeDefs: gql(fs.readFileSync('./src/schema.graphql', 'utf8')),
@@ -11,8 +8,6 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-server.applyMiddleware({ app });
-
-app.listen(4002, async () => {
-  console.log(`🚀 RUNNING @ ${4002}`);
+server.listen({ port: 4002 }).then(({ url }) => {
+  console.log(`🚀  ${url}`);
 });
